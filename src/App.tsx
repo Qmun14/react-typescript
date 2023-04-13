c76374a995e90b38e10027af258c45cbb92bb530
Qmun14 <mamunramdhan@gmail.com>
2023-04-13 14:39:39 +0700

    Basic Props --> typing Object Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 
 function App() {
+
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+
   return (
     <div className="App">
       <Greet name="Ma'mun" messageCount={15} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
