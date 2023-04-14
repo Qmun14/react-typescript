ed6730b02348799d08cdb0e02702b8cf976fa993
Qmun14 <mamunramdhan@gmail.com>
2023-04-14 22:27:02 +0700

    Prop Types and Tips ==> destructuring props, export import types dari file terpisah, reusability type dengan memanfatkan type yang sama jenis dan bentuknya untuk menghindari duplikasi code

--- src/components/Oscar.tsx
+++ src/components/Oscar.tsx
@@ -2,8 +2,8 @@ type OscarProps = {
   children: React.ReactNode
 }
 
-export const Oscar = (props: OscarProps) => {
+export const Oscar = ({ children }: OscarProps) => {
   return (
-    <div>{props.children}</div>
+    <div>{children}</div>
   )
 }
