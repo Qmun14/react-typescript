59432f79346bdf7790cce6987cff8a137999ef0b
Qmun14 <mamunramdhan@gmail.com>
2023-04-23 18:24:45 +0700

    Implementasi dari Generic Props

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { User } from './components/state/User';
 import { Counter } from './components/class/Counter'
 import { Private } from './components/auth/Private';
 import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
 
 function App() {
 
@@ -61,6 +62,25 @@ function App() {
       <MutableRef />
       <Counter message='Nilai Counter adalah' />
       <Private isLoggedIn={true} component={Profile} />
+      {/* <List items={['Batman', 'Superman', 'Wonder Woman']} onClick={item => console.log(item)} />
+      <List items={[1, 2, 3]} onClick={item => console.log(item)} /> */}
+      <List items={[
+        {
+          id: 1,
+          first: 'Bruce',
+          last: 'Wayne'
+        },
+        {
+          id: 2,
+          first: 'Clark',
+          last: 'Kent'
+        },
+        {
+          id: 3,
+          first: 'Princess',
+          last: 'Diana'
+        }
+      ]} onClick={item => console.log(item)} />
     </div>
   );
 }
