a3cf1791af51962d14c38b22bb7c2542220c294d
Qmun14 <mamunramdhan@gmail.com>
2023-04-24 10:23:44 +0700

    Extracting a Components Prop Types ==> Adakalanya Anda memerlukan type props dari sebuah komponen, tetapi type props tersebut tidak diekspor. Penyelesaian yang sederhana adalah dengan menggunakan React.ComponentProps untuk mengekstak type props-nya

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { List } from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber';
 import { Toast } from './components/templateliterals/Toast';
 import { CustomButton } from './components/html/Button';
+import { CustomComponent } from './components/html/CustomComponent';
 
 function App() {
 
@@ -89,6 +90,7 @@ function App() {
       <CustomButton variant='primary' onClick={() => console.log('click')}>
         Primary Button
       </CustomButton>
+      <CustomComponent name='Qmun' isLoggedIn={false} />
     </div>
   );
 }
